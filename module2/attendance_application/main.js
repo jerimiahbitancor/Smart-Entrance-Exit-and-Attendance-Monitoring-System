eb697291a4cf33756792d308aa62415752156fa1
LynnCzyla <lynnzylameczdo@gmail.com>
2026-05-18 00:33:05 +0800

    feature: error electron

--- module2/attendance_application/main.js
+++ module2/attendance_application/main.js
@@ -1,12 +1,46 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, session } = require('electron');
+const path = require('path');
+
+// Set custom user data
+app.setPath('userData', path.join(__dirname, 'electron-data'));
+
+// Critical: Enable Node.js integration fully
+app.commandLine.appendSwitch('enable-features=SharedArrayBuffer');
+app.commandLine.appendSwitch('disable-features=OutOfBlinkCors');
 
 function createWindow() {
   const win = new BrowserWindow({
-    width: 1000,
+    width: 1200,
     height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      webSecurity: false,
+      allowRunningInsecureContent: true,
+    },
   });
 
-  win.loadURL('http://localhost:3000'); // your React app
+  // Inject polyfill BEFORE page loads
+  win.webContents.on('did-start-loading', async () => {
+    await win.webContents.executeJavaScript(`
+      // Direct require from Electron
+      const { TextEncoder, TextDecoder } = require('util');
+      global.TextEncoder = TextEncoder;
+      global.TextDecoder = TextDecoder;
+      window.TextEncoder = TextEncoder;
+      window.TextDecoder = TextDecoder;
+      console.log('Polyfills injected!');
+    `);
+  });
+
+  win.loadURL('http://localhost:3000');
+  win.webContents.openDevTools();
+  win.maximize();
 }
 
-app.whenReady().then(createWindow);
+app.whenReady().then(() => {
+  session.defaultSession.setPermissionRequestHandler((webContents, permission, callback) => {
+    callback(permission === 'media');
+  });
+  createWindow();
+});
